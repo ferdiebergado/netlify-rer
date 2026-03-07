faa92c3ea873645b784a4b14280d26d5e02dc5c8
ferdie bergado <ferdiebergado@gmail.com>
2026-03-07 16:37:21 +0800

    chore: Add unicorn eslint plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,29 @@
 
 import eslint from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import unicorn from 'eslint-plugin-unicorn';
 import { defineConfig } from 'eslint/config';
+import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   { ignores: ['dist'] },
   {
     files: ['src/**/*.ts'],
-    extends: [eslint.configs.recommended, tseslint.configs.strict, prettier],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.strict,
+      unicorn.configs.recommended,
+      prettier,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-anonymous-default-export': 'off',
+      'unicorn/filename-case': 'off',
+    },
   },
 ]);
