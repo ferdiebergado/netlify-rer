5763e7808363d4c0bbc1c2199a8114685ab795ec
ferdie bergado <ferdiebergado@gmail.com>
2026-03-08 10:35:17 +0800

    refactor: Use element selector helper

--- src/main.ts
+++ src/main.ts
@@ -2,47 +2,46 @@ import firstNamesData from './assets/firstnames.txt?raw';
 import lastNamesData from './assets/lastnames.txt?raw';
 import './style.css';
 
-import { generateDriver, generatePlate, parseData } from './utils';
+import { generateDriver, generatePlate, getEl, parseData } from './utils';
 
 const firstNames = parseData(firstNamesData);
 const lastNames = parseData(lastNamesData);
 
-const regionSelect = document.querySelector<HTMLSelectElement>('#region');
-const generateBtn = document.querySelector<HTMLButtonElement>('#generate');
-const errorDiv = document.querySelector<HTMLDivElement>('#error');
-const plateDiv = document.querySelector<HTMLDivElement>('#plate');
-const driverDiv = document.querySelector<HTMLDivElement>('#driver');
+const regionSelect = getEl<HTMLSelectElement>('#region');
+const generateBtn = getEl<HTMLButtonElement>('#generate');
+const errorDiv = getEl<HTMLDivElement>('#error');
+const plateDiv = getEl<HTMLDivElement>('#plate');
+const driverDiv = getEl<HTMLDivElement>('#driver');
+const resultDiv = getEl<HTMLOutputElement>('#result');
 
-regionSelect?.addEventListener('change', generate);
-generateBtn?.addEventListener('click', generate);
-globalThis.addEventListener('DOMContentLoaded', generate);
+regionSelect.addEventListener('change', generate);
+generateBtn.addEventListener('click', generate);
 
-function generate(e: Event) {
-  e.preventDefault();
+generate();
 
-  if (errorDiv) errorDiv.style.display = 'none';
+function generate(e?: Event) {
+  e?.preventDefault();
+
+  errorDiv.classList.add('hidden');
+  errorDiv.textContent = '';
 
   try {
-    if (plateDiv) {
-      const plate = generatePlate(regionSelect?.value);
-      plateDiv.textContent = plate;
-    }
+    const region = regionSelect.value;
+    const plate = generatePlate(region);
+    plateDiv.textContent = plate;
+    const driver = generateDriver(firstNames, lastNames);
+    driverDiv.textContent = driver;
 
-    if (driverDiv) {
-      const driver = generateDriver(firstNames, lastNames);
-      driverDiv.textContent = driver;
-    }
+    resultDiv.animate([{ opacity: 0.1 }, { opacity: 1 }], { duration: 700 });
   } catch (error) {
-    handleError(error);
+    showError(error);
   }
 }
 
-function handleError(error: unknown) {
-  console.error(error);
+function showError(error: unknown) {
+  console.error('[Generator Error]:', error);
 
-  if (errorDiv) {
-    const err = error instanceof Error ? error.message : String(error);
-    errorDiv.textContent = err;
-    errorDiv.style.display = 'block';
-  }
+  const msg = error instanceof Error ? error.message : 'An unexpected error occurred.';
+  errorDiv.textContent = msg;
+  errorDiv.classList.remove('hidden');
 }
